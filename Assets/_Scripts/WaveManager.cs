fcbb210d3ddbbaab6822b04b5c8a3bcaf077e5dc
Toreole <toreole.official@gmail.com>
2019-11-27 15:07:43 +0100

    Added WaveManager
    no work on it tho really, honestly not sure how to do this

---
+++ Assets/_Scripts/WaveManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LATwo
+{
+    public class WaveManager : MonoBehaviour
+    {
+        public Wave[] waves;
+
+        protected int totalEnemies;
+
+        private void Start()
+        {
+            foreach (var x in waves)
+                totalEnemies += x.amount;
+        }
+
+        //this is what spawns the waves and detects whether its been completed yet.
+        //waves spawn after time.
+        //all waves done => next level
+        [System.Serializable]
+        public class Wave
+        {
+            //not sure about this one chief.
+            public Enemy enemyType;
+            public int amount;
+            public float time;
+        }
+    }
+}
