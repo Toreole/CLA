84edd880dfb7c4389957776d3363019415119580
Toreole <toreole.official@gmail.com>
2020-01-31 20:01:42 +0100

    Build

--- Assets/_Scripts/PlayButton.cs
+++ Assets/_Scripts/PlayButton.cs
@@ -5,5 +5,6 @@ namespace LATwo
     public class PlayButton : MonoBehaviour
     {
         public void StartGame() => GameManager.StartGame();
+        public void QuitGame() => Application.Quit();
     }
 }
